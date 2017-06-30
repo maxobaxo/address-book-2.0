e1b99d10d5c7712462e177f1e60b926363e2f660
Max Scher <maxobaxo@gmail.com>
2017-06-30 11:37:42 -0700

    fix syntax in Contact.php to avoid undefined properties

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__.'/../vendor/autoload.php';
-    require_once __DIR__.'/../src/class.php';
+    require_once __DIR__.'/../src/Class.php';
 
     session_start();
 
@@ -14,14 +14,12 @@
     ));
 
     $app->get('/', function() use ($app) {
-        return $app['twig']->render('home.html.twig');
+        return $app['twig']->render('home.html.twig', array('contacts' => Contact::getAll()));
     });
 
     $app->post('/create_contact', function() use ($app) {
-        var_dump($_POST['last_name']);
         $contact = new Contact($_POST['first_name'], $_POST['last_name'], $_POST['street_num'], $_POST['city'], $_POST['state'], $_POST['zip'], $_POST['mobile'], $_POST['email']);
 
-        var_dump($contact);
         $contact->save();
         return $app['twig']->render('create.html.twig', array('new_contact' => $contact));
     });
--- src/class.php
+++ src/class.php
@@ -11,94 +11,95 @@
 
         function __construct($first_name, $last_name, $street_num, $city, $state, $zip, $mobile, $email)
         {
-            $this->$first_name = $first_name;
-            $this->$last_name = $last_name;
-            $this->$street_num = $street_num;
-            $this->$city = $city;
-            $this->$state = $state;
-            $this->$zip = $zip;
-            $this->$mobile = $mobile;
-            $this->$email = $email;
+            $this->first_name = $first_name;
+            $this->last_name = $last_name;
+            $this->street_num = $street_num;
+            $this->city = $city;
+            $this->state = $state;
+            $this->zip = $zip;
+            $this->mobile = $mobile;
+            $this->email = $email;
         }
 
         function setFirstName($first_name)
         {
-            $this->$first_name = (string) $first_name;
+            $this->first_name = (string) $first_name;
         }
 
         function getFirstName()
         {
-            return $this->$first_name;
+            return $this->first_name;
         }
 
         function setLastName($last_name)
         {
-            $this->$last_name = (string) $last_name;
+            $this->last_name = (string) $last_name;
         }
 
         function getLastName()
         {
-            return $this->$last_name;
+            return $this->last_name;
         }
 
         function setStreet($street_num)
         {
-            $this->$street_num = (string) $street_num;
+            $this->street_num = (string) $street_num;
         }
 
         function getStreet()
         {
-            return $this->$street_num;
+            return $this->street_num;
         }
 
         function setCity($city)
         {
-            $this->$city = (string) $city;
+            $this->city = (string) $city;
         }
 
         function getCity()
         {
-            return $this->$city;
+            return $this->city;
         }
 
         function setState($state)
         {
-            $this->$state = (string) $state;
+            $this->state = (string) $state;
         }
 
         function getState() {
-            return $this->$state;
+            return $this->state;
         }
 
         function setZip($zip) {
-            $this->$zip = (string) $zip;
+            $this->zip = (string) $zip;
         }
 
         function getZip() {
-            return $this->$zip;
+            return $this->zip;
         }
 
         function setEmail($email)
         {
-            $this->$email = (string) $email;
+            $this->email = (string) $email;
         }
 
         function getEmail()
         {
-            return $this->$email;
+            return $this->email;
         }
 
         function setMobile($mobile)
         {
-            $this->$mobile = (string) $mobile;
+            $this->mobile = (string) $mobile;
         }
 
         function getMobile()
         {
-            return $this->$mobile;
+            return $this->mobile;
         }
 
-        function save() {
+        function save()
+        {
             array_push($_SESSION['contacts'], $this);
         }
 
