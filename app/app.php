50a5a240116d3df87f5edcf31237ee4bab16793f
Max Scher <maxobaxo@gmail.com>
2017-06-30 08:19:29 -0700

    add  with class setters and getters

--- app/app.php
+++ app/app.php
@@ -3,6 +3,12 @@
     require_once __DIR__.'/../vendor/autoload.php';
     require_once __DIR__.'/../src/class.php';
 
+    session_start();
+
+    if (empty($_SESSION['contacts'])) {
+        $_SESSION['contacts'] = array();
+    }
+
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'
     ));
--- src/class.php
+++ src/class.php
@@ -21,5 +21,81 @@
             $this->$mobile = $mobile;
             $this->$email = $email;
         };
+
+        function setFirstName($first_name) {
+            $this->$first_name = $first_name;
+        }
+
+        function getFirstName() {
+            return $this->$first_name;
+        }
+
+        function setLastName($last_name) {
+            $this->$last_name = $last_name;
+        }
+
+        function getLastName() {
+            return $this->$last_name;
+        }
+
+        function setStreet($street_num) {
+            $this->$street_num = $street_num;
+        }
+
+        function getStreet() {
+            return $this->$street_num;
+        }
+
+        function setCity($city) {
+            $this->$city = $city;
+        }
+
+        function getCity() {
+            return $this->$city;
+        }
+
+        function setState($state) {
+            $this->$state = $state;
+        }
+
+        function getState() {
+            return $this->$state;
+        }
+
+        function setZip($zip) {
+            $this->$zip = $zip;
+        }
+
+        function getZip() {
+            return $this->$zip;
+        }
+
+        function setEmail($email) {
+            $this->$email = $email;
+        }
+
+        function getEmail() {
+            return $this->$email;
+        }
+
+        function setMobile($mobile) {
+            $this->$mobile = $mobile;
+        }
+
+        function getMobile() {
+            return $this->$mobile;
+        }
+
+        function save() {
+            array_push($_SESSION['contacts'], $this);
+        }
+
+        static function getAll() {
+            return $_SESSION['contacts'];
+        }
+
+        static function deleteAll() {
+            $_SESSION['contacts'] = array();
+        }
     }
 ?>
